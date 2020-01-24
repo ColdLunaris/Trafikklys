0195219fe5dcb63e21635a429301be492f4c19e8
Lunaris <azurefrost@protonmail.com>
2020-01-24 14:42:14 +0100

    Ferdig oppgave 8 Frikk

--- Trafikklys_Frikk_Oppgave_8.ino
+++ Trafikklys_Frikk_Oppgave_8.ino
@@ -1,17 +1,12 @@
 const int redPin = 10;          // nummeret til den røde LED pinnen
 const int yellowPin = 9;        // nummeret til den gule LED pinnen
 const int greenPin = 8;         // nummeret til den grønne LED pinnen
-const int redPedPin = 3;        // nummeret til den røde fotgjengerfelt LED pinnen
-const int greenPedPin = 2;      // nummeret til den grønne fotgjengerfelt LED pinnen
-const int button = 12;          // nummeret til knappen som aktiverer fotgjengerfelt
 
 // Variabler som blir endret:
 
 int redState = LOW;             // redState er brukt til hva lyset er før koden sier noe annet 
 int yellowState = LOW;          // yellowState er brukt til hva lyset er før koden sier noe annet 
 int greenState = LOW;           // greenState er brukt til hva lyset er før koden sier noe annet
-int redPedState = LOW;          // redPedState er brukt til hva fotgjengerfelt-lyset er før koden sier noe annet
-int greenPedState = LOW;        // greenPedState er brukt til hva fotgjengerfelt-lyset er før koden sier noe annet
 long StartTime = 0;       
 
 long interval = 19000;           // dette er hvor lenge denne syklysen totalt skal vare. 
@@ -21,11 +16,6 @@ void setup() {
     pinMode(redPin, OUTPUT);
     pinMode(yellowPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
-    pinMode(redPedPin, OUTPUT);
-    pinMode(greenPedPin, OUTPUT);
-
-    // her sier koden at knappen skal være en input med intern resistans:
-    pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
@@ -68,6 +58,4 @@ void loop() {
     digitalWrite(redPin, redState);
     digitalWrite(greenPin, greenState);
     digitalWrite(yellowPin, yellowState);
-    digitalWrite(redPedPin, redPedState);
-    digitalWrite(greenPedPin, greenPedState);
 }
